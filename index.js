37c99c64d1d8d7a2500a05337f210d111e84210e
Meg Galvez <meg@oakmaine.com>
2019-11-21 16:50:58 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+function saturdayFun(activity='roller-skate'){
+   return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work = 'go to the office'){
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(flair = "*") {
+    let visualFlair = flair 
+    return function(param="special"){
+        return `You are ${visualFlair}${param}${visualFlair}!`
+    }
+}
+
+const Calculator = {
+    add: function(a, b) {
+       return a+b
+    },
+    subtract: function(a, b) {
+        return a-b
+    },
+    multiply: function(a, b) {
+       return a*b
+    },
+    divide: function(a, b) {
+       return a/b
+    }
+}
+
+function actionApplyer(start, array) {
+    let totalInt = start
+    if (array.length === 0 )
+        return start 
+    else  array.forEach(func => {
+
+        totalInt = func(start)
+        start = totalInt
+      })
+      return totalInt
+}
