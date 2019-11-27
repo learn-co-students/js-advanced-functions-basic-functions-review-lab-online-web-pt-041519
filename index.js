c84b15eb1b7d02715eb60194abb6312efe474ec1
Renee Nordholm <reneenordholm@gmail.com>
2019-11-26 20:33:02 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+function saturdayFun(param = 'roller-skate') {
+   return `This Saturday, I want to ${param}!`
+}
+
+let mondayWork = function(param = "go to the office") {
+    return `This Monday, I will ${param}.`
+}
+
+let wrapAdjective = function(string = "*") {
+    return function(param = "special") {
+        return `You are ${string}${param}${string}!`
+    }
+}
+
+let encouragingPromptFunction = wrapAdjective("!!!")
+
+let Calculator = {
+    add() {
+        return 1 + 3
+    },
+    subtract() {
+        return 1 - 3
+    },
+    multiply() {
+        return 1 * 3
+    },
+    divide() {
+        return 10 / 5
+    }
+}
+
+function actionApplyer(integer, array) {
+    for (let i = 0; i < array.length; i++) {
+        integer = array[i](integer)
+    }
+    return integer
+}
